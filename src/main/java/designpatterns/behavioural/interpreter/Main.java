ff8abdaf62e4cf488cec53c957a11e004e7cba98
hakan dereli <hakandereli28@gmail.com>
2023-07-04 23:20:09 +0300

    added interpreter comments

--- src/main/java/designpatterns/behavioural/interpreter/Main.java
+++ src/main/java/designpatterns/behavioural/interpreter/Main.java
@@ -1,4 +1,37 @@
 package designpatterns.behavioural.interpreter;
 
 public class Main {
+
+    //Yorumlayıcı tasarım kalıbı demektir türkçesi.
+
+    //Interpreter tasarım kalıbı, bir dilin belirli bir gramerini veya sözdizimini yorumlayan ve bu dildeki ifadeleri
+    // işleyen bir yapısal tasarım kalıbıdır.
+    // Bu tasarım kalıbı, bir dili temsil eden bir dilbilgisi ağacı oluşturur ve ardından bu ağacı dolaşarak
+    // ifadeleri yorumlar.
+    //
+    //Interpreter tasarım kalıbı, bir dilin ifadelerini işlemek ve yorumlamak için kullanılır. Özellikle, metin
+    // tabanlı bir dilin derlenmesi veya yorumlanması gereken durumlarda kullanışlıdır.
+    // Bu tasarım kalıbı, derleme veya yorumlama sürecini daha modüler hale getirerek,
+    // dilin gramerindeki değişikliklere daha kolay adapte olmayı sağlar.
+    //
+    //Interpreter tasarım kalıbı, sözdizimsel analiz ve semantik işlemleri ayırarak karmaşık bir dilin yapısını
+    // kolaylaştırır. Ayrıca, yeni ifadelerin eklenmesi veya mevcut ifadelerin değiştirilmesi gibi dildeki
+    // değişikliklerin etkilerini izole etmek için kullanılır. Bu sayede, yorumlama sürecindeki hataların
+    // giderilmesi ve dilin farklı varyasyonlarının desteklenmesi daha kolay hale gelir.
+    //
+    //Özetle, Interpreter tasarım kalıbı, bir dilin ifadelerini yorumlamak ve işlemek için kullanılır.
+    // Dilin gramerini temsil eden bir yapı oluşturarak, derleme veya yorumlama sürecini modülerleştirir ve
+    // dildeki değişikliklere daha kolay adapte olmayı sağlar.
+
+//    Interpreter tasarım kalıbının bazı kullanım senaryoları:
+
+//    Programlama dilleri veya komut dilleri yorumlaması: Eğer kendi programlama dilinizi veya komut dilinizi geliştiriyorsanız, bu dilin yorumlanması veya derlenmesi için Interpreter tasarım kalıbını kullanabilirsiniz. Örneğin, bir betik dili veya komut tabanlı bir dilin ifadelerini yorumlamak için bu kalıbı kullanabilirsiniz.
+
+//    Matematiksel ifadelerin değerlendirilmesi: Matematiksel ifadeleri yorumlamak veya değerlendirmek için Interpreter tasarım kalıbını kullanabilirsiniz. Bu sayede, kullanıcı tarafından girilen matematiksel ifadeleri anlayabilir ve sonuçlarını hesaplayabilirsiniz. Örneğin, bir hesap makinesi uygulamasında kullanıcı tarafından girilen matematiksel ifadeleri yorumlamak için bu kalıbı kullanabilirsiniz.
+
+//    Veri formatlarının yorumlanması: Bazı durumlarda, özel bir veri formatını yorumlamak ve bu formata göre işlem yapmak gerekebilir. Örneğin, bir belge formatını yorumlamak veya bir veritabanı sorgusunu işlemek için Interpreter tasarım kalıbını kullanabilirsiniz.
+
+//    Doğal dil işleme: Doğal dil işleme alanında, metin tabanlı dilin yorumlanması veya analizi gerekebilir. Örneğin, metin tabanlı komutları anlamak veya doğal dildeki ifadeleri işlemek için Interpreter tasarım kalıbını kullanabilirsiniz.
+
+//    Interpreter tasarım kalıbı, karmaşık bir dilin yapısını modülerleştirerek, dilin gramerindeki değişikliklere daha kolay adapte olmanızı sağlar. Ayrıca, yeni ifadelerin eklenmesi veya mevcut ifadelerin değiştirilmesi gibi dildeki değişikliklerin etkilerini izole etmek için de kullanışlıdır. Bu, yazılım dünyasında farklı yorumlama veya derleme ihtiyaçları olan senaryolarda kullanılabilecek bir tasarım kalıbıdır.
 }
